753860f925d99edf79402bbac2692156d9fdecb3
Raul <raulsanchez1024@gmail.com>
2018-09-17 12:37:02 -0600

    done

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -7,10 +9,13 @@ import java.util.Scanner;
 
 public class Driver {
 	
-	static void insertionSort() throws IOException {
+	static void insertionSort(String f) throws IOException, URISyntaxException {
 //		get file, read file, and store the text into an array
 		
-		File file = new File("/Users/raulsanchez/Desktop/COMP 2370/Project1/src/istest3.txt"); 
+		URL url = ClassLoader.getSystemResource(f);
+
+		
+		File file = new File(url.toURI()); 
 	    Scanner sc = new Scanner(file); 
 	    			  
 //	    insert all words into an ArrayList (arrList)
@@ -35,16 +40,35 @@ public class Driver {
         }
         arr[i + 1] = key;
     }
+//	    reverse arr
 	    Collections.reverse(Arrays.asList(arr));
-	    System.out.println(Arrays.toString(arr));
+//	    print out
+	    for(int i = 0; i < arr.length; i++) {
+	    	System.out.println(arr[i]);
+	    }
 
 	}
 
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, URISyntaxException {
 		
-		insertionSort();
+		String f1 = args[0];
+		String f2 = args[1];
+		String f3 = args[2];
 		
+//		Uncomment each block to run test
+//		f1 equals istest1.txt
+//		f2 equals istest2.txt
+//		f3 equals istest3.txt
+		
+		System.out.println(" ===== File one (istest1.txt) ===== ");
+		insertionSort(f1);
+
+//		System.out.println(" ===== File two (istest2.txt) ===== ");
+//		insertionSort(f2);
+		
+//		System.out.println(" ===== File three (istest3.txt) ===== ");
+//		insertionSort(f3);
 
 	}
 
